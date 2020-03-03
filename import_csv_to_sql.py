41d5a6ab5ea6d00d0629ad9528243bc7c03bf068
Supermomie <fakhredinedev3.0@gmail.com>
2020-03-03 01:20:49 +0100

    clean script

--- import_csv_to_sql.py
+++ import_csv_to_sql.py
@@ -4,10 +4,12 @@ import sqlalchemy
 import pymysql
 
 start = time.time()
+
+
 # Configuration serveur
-databaseServerIP      = "localhost" #OR 127.0.0.1
-databaseUserName      = "root"
-databaseUserPassword  = ""
+DATABASESERVERIP      = "localhost" #OR 127.0.0.1
+DATABASEUSERNAME      = "root"
+DATABASEUSERPASSWORD  = ""
 
 # Nom de la base de donnee et nom de la table
 DB_NAME    = ''
@@ -20,24 +22,39 @@ PATHFILE   = ''
 NAME       = ''
 
 ENCODING = 'utf-8'
+SEP      = ';'
 
-engine = sqlalchemy.create_engine('mysql+pymysql://'+databaseUserName+':@'+databaseServerIP+'/'+DB_NAME)
+def connection(databaseUserName, databaseServerIP, dbName):
+    # Connection a mysql
+    engine = sqlalchemy.create_engine('mysql+pymysql://'+databaseUserName+':@'+databaseServerIP+'/'+ dbName)
+    connectionInstance = pymysql.connect(host=databaseServerIP, user=databaseUserName, password=databaseUserPassword,charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
+
+def create_DB(dbName):
+    # Instructions SQL
+    cursorInsatnce = connectionInstance.cursor()
+    sqlStatement = "CREATE DATABASE "+ dbName
+    cursorInsatnce.execute(sqlStatement)
 
 
-connectionInstance = pymysql.connect(host=databaseServerIP, user=databaseUserName, password=databaseUserPassword,charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
-cursorInsatnce = connectionInstance.cursor()
-sqlStatement = "CREATE DATABASE "+DB_NAME
-cursorInsatnce.execute(sqlStatement)
-
-print("IMPORT DATABASE")
-data = pd.read_csv(PATHFILE+'/'+NAME, encoding=ENCODING, sep=';', dtype='object')
-columnsName = [col for col in data.columns]
-ratings = pd.read_csv(PATHFILE+'/'+NAME, sep=';', encoding=ENCODING, usecols=columnsName, dtype='object')
+def read_CSV(pathfile, name, encoding, sep):
+    # Lecture du fichier csv
+    data = pd.read_csv(pathfile +'/'+ name, encoding=encoding, sep=sep, dtype='object')
+    columnsName = [col for col in data.columns]
+    ratings = pd.read_csv(pathfile +'/'+ name, encoding=encoding, usecols=columnsName, sep=sep, dtype='object')
 
 
-ratings.to_sql(TABLE_NAME, con=engine, if_exists='append', index=False, chunksize=1)
+def all_process(databaseUserName, databaseServerIP, dbName, pathfile, name, encoding, sep):
+    print("IMPORT DATABASE")
+    connection(DATABASEUSERNAME, databaseServerIP, dbName)
+    createDB(dbName)
+    readCsv(pathfile, name, encoding, sep)
 
-print("IMPORT TERMINER")
+    # Execution de tralala
+    ratings.to_sql(TABLE_NAME, con=engine, if_exists='append', index=False, chunksize=1)
 
+    print("IMPORT TERMINER")
+
+
+all_process(DATABASEUSERNAME, DATABASESERVERIP, DB_NAME, PATHFILE, NAME, ENCODING, SEP)
 end = time.time()
-print("import time {}".format(end-start))
+print("time {}".format(end-start))
