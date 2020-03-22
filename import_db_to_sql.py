b38a4a678aeba286eacb69f1ac8a297072199eab
Supermomie <fakhredinedev3.0@gmail.com>
2020-03-22 10:24:49 +0100

    finish convert regex by pathlib

--- import_db_to_sql.py
+++ import_db_to_sql.py
@@ -6,45 +6,33 @@ import sqlalchemy
 import pymysql
 from sys import argv
 from termcolor import colored
-
 from pathlib import Path
 
 start = time.time()
 
-#TODO CONVERT ALL REGEX BY PATHLIB
 # Configuration serveur
 DATABASESERVERIP      = "localhost" #OR 127.0.0.1
 DATABASEUSERNAME      = "root"
 DATABASEUSERPASSWORD  = ""
 
 # Nom de la base de donnee et nom de la table
-
 DB_NAME    = argv[2]
-
 
 # Chemin du ficher CSV
 PATHFILE   = argv[1]
 
 # Nom de la colone
 TABLE_NAME = Path(PATHFILE).stem
-#TABLE_NAME = re.search("([^/][^/]+\.)", PATHFILE)[0]
-print(TABLE_NAME)
-exit()
-
 
 # Nom du fichier
-win = re.search("([^\\\]+$)", PATHFILE)[0]
-linux = re.search("([^\/]+$)", PATHFILE)[0]
+FILE_NAME = os.path.basename(PATHFILE)
 
-EXTENSION  = re.search('([^.]+$)', PATHFILE)[0]
+EXTENSION  = Path(PATHFILE).suffix
 
+# Encodage et Separateur
 ENCODING = 'utf-8'
 SEP      = ','
 
-def check_Os(win, linux):
-    return os.system(win if os.name == 'nt' else linux)
-
-NAME = check_Os(win, linux)
 
 
 def connection(databaseUserName, databaseServerIP, databaseUserPassword, dbName):
@@ -71,19 +59,17 @@ def create_DB(databaseUserName, databaseServerIP, databaseUserPassword, dbName):
     return cursorInsatnce
 
 
-def read_CSV(pathfile, name, encoding, sep):
+def read_CSV(pathfile, encoding, sep):
     # Lecture du fichier csv
-    data = pd.read_csv(pathfile +'/'+ name, encoding=encoding, sep=sep, dtype='object')
+    data = pd.read_csv(pathfile, encoding=encoding, sep=sep)
     columnsName = [col for col in data.columns] # OR columnsName = data.columns
-    ratings = pd.read_csv(pathfile +'/'+ name, encoding=encoding, 
-                            usecols=columnsName, sep=sep, dtype='object')
+    ratings = pd.read_csv(pathfile, encoding=encoding, 
+                            usecols=columnsName, sep=sep)
     return ratings
 
 
-def read_JSON(pathfile, name):
+def read_JSON(pathfile):
     # Lecture du ficher json
-    print(pathfile)
-    print(name)
     data = pd.read_json(pathfile)
     return data
 
@@ -96,7 +82,7 @@ def error_MSG():
 
 
 def all_process(databaseUserName, databaseServerIP, databaseUserPassword, 
-                            tableName, dbName, pathfile, name, encoding, sep):
+                            tableName, dbName, pathfile, encoding, sep):
     if len(argv) <= 1:
         print(error_MSG())
         exit()
@@ -105,12 +91,12 @@ def all_process(databaseUserName, databaseServerIP, databaseUserPassword,
     engine, _ = connection(databaseUserName, databaseServerIP, databaseUserPassword, dbName)
     create_DB(databaseUserName, databaseServerIP, databaseUserPassword, dbName)
     
-    if EXTENSION == "csv":
-        ratings = read_CSV(pathfile, name, encoding, sep)
+    if EXTENSION == ".csv":
+        ratings = read_CSV(pathfile, encoding, sep)
         ratings.to_sql(tableName, con=engine, if_exists='append', index=False, chunksize=1)
     
-    if EXTENSION == "json":
-        data = read_JSON(pathfile, name)
+    if EXTENSION == ".json":
+        data = read_JSON(pathfile)
         data.to_sql(tableName, con=engine, if_exists='append', index=True, chunksize=1)
     
     print("IMPORT TERMINER")
@@ -118,7 +104,7 @@ def all_process(databaseUserName, databaseServerIP, databaseUserPassword,
 
 
 all_process(DATABASEUSERNAME, DATABASESERVERIP, DATABASEUSERPASSWORD, 
-                TABLE_NAME, DB_NAME, PATHFILE, NAME, ENCODING, SEP)
+                TABLE_NAME, DB_NAME, PATHFILE, ENCODING, SEP)
 
 
 
