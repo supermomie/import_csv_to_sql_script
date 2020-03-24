0375cad03f11e70c3e901520b3f216955b0c6994
Supermomie <fakhredinedev3.0@gmail.com>
2020-03-24 01:29:04 +0100

    change to class

--- menu.py
+++ menu.py
@@ -1,28 +1,43 @@
 import os
 from sys import argv
 from termcolor import colored
+from cmdarg import CmdArg
 
 
-
-mandatoryArg = colored("?", "green", attrs=["bold"])
-
-def men():
-    arguments = {
-        'n:': 'nom',
-        'c:': 'chemin',
-    }
-    if len(argv) == 1 or len(argv) <= 1:
-        print(colored("                            Mode d'emploi :                                \n\n", "red", attrs=['bold', 'reverse'])
-        + colored(os.getcwd(), "red", attrs=["bold"])+ " [" + colored("-d", "red") + "=" + colored("\"directory\"", "yellow") +"] [" + colored("-tn", "red") + "=" + colored("\"table name\"", "yellow" ) + "] [" + colored("-cn", "red") + "=" + colored("\"column name\"", "yellow" ) + "]"  + " [" + colored("-a", "red") + "]"+ mandatoryArg +"\n\n"
-        
-        
-        + colored("Arguments : \n\n", "green")
-        + colored("  -d  / --dir", "cyan") + "         : Absolute path of file/folder " + colored("(string) \n", "blue")
-        + colored("  -tn / --table_name", "cyan") + "  : Database Name " + colored("(string) \n", "blue")
-        + colored("  -cn / --column_name", "cyan") + " : Column name " + colored("(string) \n", "blue")
-        + colored("  -a  / --all", "cyan") + "         : All compatible files in folder " + colored("(string) \n", "blue")
-        )
-        exit()
-    error = []
+class menu():
     
-men()
+    def __init__(self, arg):
+        return arg == self.men(arg)
+
+    def men(self, arg):
+        arguments = arg
+        if len(argv) == 1 or len(argv) <= 1:
+            mandatoryArg = colored("?", "green", attrs=["bold"])
+            print(colored("                            Mode d'emploi :                                \n\n", "red", attrs=['bold', 'reverse'])
+            + colored(os.getcwd(), "red", attrs=["bold"])+ " [" + colored("-d", "red") + "=" + colored("\"directory\"", "yellow") +"] [" + colored("-tn", "red") + "=" + colored("\"table name\"", "yellow" ) + "] [" + colored("-cn", "red") + "=" + colored("\"column name\"", "yellow" ) + "]"  + " [" + colored("-a", "red") + "]"+ mandatoryArg +"\n\n"
+            
+            
+            + colored("Arguments : \n\n", "green")
+            + colored("  -d  / --dir", "cyan") + "         : Absolute path of file/folder " + colored("(string) \n", "blue")
+            + colored("  -tn / --table_name", "cyan") + "  : Database Name " + colored("(string) \n", "blue")
+            + colored("  -cn / --column_name", "cyan") + " : Column name " + colored("(string) \n", "blue")
+            + colored("  -a  / --all", "cyan") + "         : All compatible files in folder " + colored("(string) \n", "blue")
+            )
+            exit()
+        error = []
+        cmdarg = CmdArg(arguments, argv)
+        args = cmdarg.check(error);
+        print("args", args)
+        print("error ", error)
+        if args is not None:
+            if args[0] == -2:
+                print(colored("The argument " +args[1]+" is incorect", "red"))
+                exit()
+            elif args == -1:
+                return -3
+            return args
+
+
+arg = ['d', 'tn', 'cn', 'a']
+m = menu(arg)
+ar = m.men()
